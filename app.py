ad3875c0ccaee4bc4bc0225ab6f1660f12a1d7b8
EricBollar <esbollar@gmail.com>
2020-06-03 15:32:53 -0700

    removed /handle_data

--- app.py
+++ app.py
@@ -20,14 +20,6 @@ def test():
     print(tupl[1])
     return jsonify(tupl)
 
-
-@app.route('/handle_data', methods=['POST'])
-def handle_data():
-    projectpath = request.form['projectFilepath']
-    searchTerm = projectpath
-    tupl = quoteAPI.getQuoteWithKeyword(searchTerm)
-    return render_template('index.html', quote = tupl[0], author = tupl[1])
-
 if __name__ == '__main__':
     if (quoteAPI.isServer()):
         app.run(host='0.0.0.0')
